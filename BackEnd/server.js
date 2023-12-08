22d1374f9a63729ba74118595d68bd20f9df72c3
TommyMitchellATU <g00400162@atu.ie>
2023-12-08 09:46:10 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,18 +1,24 @@
+//Imports
 const express = require('express')
 const app = express()
 const port = 4000
 const cors = require('cors');
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
 
+// Enable CORS
 app.use(cors());
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-res.header("Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept");
-next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+// Use body-parser for handling JSON and URL encoded data
 const bodyParser = require("body-parser");
 
 //Here we are configuring express to use body-parser as middle-ware.
@@ -22,68 +28,76 @@ app.use(bodyParser.json());
 
 // getting-started.js
 const mongoose = require('mongoose');
-
 main().catch(err => console.log(err));
-
 async function main() {
-  await mongoose.connect('mongodb+srv://admin:admin@martinscluster.w5rtkz0.mongodb.net/DB14?retryWrites=true&w=majority');
+  await mongoose.connect('mongodb+srv://g00400162:Tuesday@cluster0.aklr9hh.mongodb.net/?retryWrites=true&w=majority');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
+// Define a book schema using Mongoose
 const bookSchema = new mongoose.Schema({
-  title:String,
-  cover:String,
-  author:String
+  title: String,
+  cover: String,
+  author: String
 })
 
+// Create a model based on the book schema
 const bookModel = mongoose.model('dfgdfgdfgdfg5r5645634fggh', bookSchema);
 
-app.delete('/api/book/:id',async (req, res)=>{
-  console.log("Delete: "+req.params.id);
+// Endpoint to delete a book
+app.delete('/api/book/:id', async (req, res) => {
+  console.log("Delete: " + req.params.id);
 
   let book = await bookModel.findByIdAndDelete(req.params.id);
   res.send(book);
 })
 
+// Endpoint to update a book
+app.put('/api/book/:id', async (req, res) => {
+  console.log("Update: " + req.params.id);
 
-app.put('/api/book/:id', async(req, res)=>{
-  console.log("Update: "+req.params.id);
-
-  let book = await bookModel.findByIdAndUpdate(req.params.id, req.body, {new:true});
+  let book = await bookModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
   res.send(book);
 })
 
+// Endpoint to create a new book
+app.post('/api/book', (req, res) => {
+  console.log(req.body);
 
-app.post('/api/book', (req,res)=>{
-    console.log(req.body);
-
-    bookModel.create({
-      title:req.body.title,
-      cover:req.body.cover,
-      author:req.body.author
-    })
-    .then(()=>{ res.send("Book Created")})
-    .catch(()=>{ res.send("Book NOT Created")});
+  bookModel.create({
+    title: req.body.title,
+    cover: req.body.cover,
+    author: req.body.author
+  })
+    .then(() => { res.send("Book Created") })
+    .catch(() => { res.send("Book NOT Created") });
 
 })
 
+// Default
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/books', async(req, res)=>{
-    
+// Get all books
+app.get('/api/books', async (req, res) => {
+
   let books = await bookModel.find({});
   res.json(books);
 })
 
-app.get('/api/book/:identifier',async (req,res)=>{
+// Get a specific book by ID
+app.get('/api/book/:identifier', async (req, res) => {
   console.log(req.params.identifier);
 
   let book = await bookModel.findById(req.params.identifier);
   res.send(book);
 })
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/../build/index.html'));
+  });  
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
